2d88e145b983343a1cfc641feaa0fdd274aa3a4e
Olivier Boukili <boukili.olivier@gmail.com>
2021-01-15 17:16:01 +0100

    Portable, cross-platform tests settings (#42)
    * persist Goland gotest templates, made Kind test settings cross-platform through extraPortMappings, removed CI testing dependencies with kubectl proxy
    
    * fix kustomize acceptance tests, updated README.md

--- scripts/testacc_prepare_env.sh
+++ scripts/testacc_prepare_env.sh
@@ -7,16 +7,25 @@ echo '--- Kustomize sanity checks'
 kustomize version || exit 1
 
 echo '--- Create Kind cluster\n\n'
-kind create cluster --name argocd
+kind create cluster --name argocd --config scripts/kind-config.yml --image kindest/node:${ARGOCD_KUBERNETES_VERSION:-v1.18.8}
 
 echo '--- Kind sanity checks\n\n'
 kubectl get nodes -o wide
 kubectl get pods --all-namespaces -o wide
 kubectl get services --all-namespaces -o wide
 
+echo '--- Load already available container images from local registry into Kind'
+kind load docker-image redis:5.0.3 --name argocd
+kind load docker-image quay.io/dexidp/dex:v2.22.0 --name argocd
+kind load docker-image argoproj/argocd:${ARGOCD_VERSION:-v1.6.1} --name argocd
+
 echo '--- Install ArgoCD ${ARGOCD_VERSION:-v1.6.1}\n\n'
 kustomize build manifests/install | kubectl apply -f - &&
 kubectl apply -f manifests/testdata/ &&
 
-echo '--- Wait for ArgoCD server to be ready...'
+echo '--- Wait for ArgoCD components to be ready...'
 kubectl wait --for=condition=available --timeout=600s deployment/argocd-server -n argocd
+kubectl wait --for=condition=available --timeout=30s deployment/argocd-repo-server -n argocd
+kubectl wait --for=condition=available --timeout=30s deployment/argocd-application-controller -n argocd
+kubectl wait --for=condition=available --timeout=30s deployment/argocd-dex-server -n argocd
+kubectl wait --for=condition=available --timeout=30s deployment/argocd-redis -n argocd
