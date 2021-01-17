b791194f0a3c351a39c3925ce869201cbaf1b8d2
Olivier Boukili <boukili.olivier@gmail.com>
2021-01-17 23:01:21 +0100

    build against Go 1.15, update local development testing ArgoCD version to 1.8.2

--- scripts/testacc_prepare_env.sh
+++ scripts/testacc_prepare_env.sh
@@ -14,10 +14,10 @@ kubectl get nodes -o wide
 kubectl get pods --all-namespaces -o wide
 kubectl get services --all-namespaces -o wide
 
-echo '--- Load already available container images from local registry into Kind'
-kind load docker-image redis:5.0.3 --name argocd
-kind load docker-image quay.io/dexidp/dex:v2.22.0 --name argocd
-kind load docker-image argoproj/argocd:${ARGOCD_VERSION:-v1.6.1} --name argocd
+echo '--- Load already available container images from local registry into Kind (local development only)'
+kind load docker-image redis:5.0.10-alpine --name argocd
+kind load docker-image ghcr.io/dexidp/dex --name argocd
+kind load docker-image argoproj/argocd:${ARGOCD_VERSION:-v1.8.2} --name argocd
 
 echo '--- Install ArgoCD ${ARGOCD_VERSION:-v1.6.1}\n\n'
 kustomize build manifests/install | kubectl apply -f - &&
