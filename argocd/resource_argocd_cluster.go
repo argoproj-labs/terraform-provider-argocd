76ae43aeb32069846049f0ffe7a759060fffb71d
Olivier Boukili <boukili.olivier@gmail.com>
2021-02-24 00:45:10 +0100

    support for name and server in cluster resource id

--- argocd/resource_argocd_cluster.go
+++ argocd/resource_argocd_cluster.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	clusterClient "github.com/argoproj/argo-cd/pkg/apiclient/cluster"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"strings"
 )
 
 func resourceArgoCDCluster() *schema.Resource {
@@ -13,7 +14,7 @@ func resourceArgoCDCluster() *schema.Resource {
 		Read:   resourceArgoCDClusterRead,
 		Update: resourceArgoCDClusterUpdate,
 		Delete: resourceArgoCDClusterDelete,
-		// TODO: add importer acceptance tests
+		// TODO: add importer tests
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
@@ -33,15 +34,18 @@ func resourceArgoCDClusterCreate(d *schema.ResourceData, meta interface{}) error
 	if err != nil {
 		return fmt.Errorf("something went wrong during cluster resource creation: %s", err)
 	}
-	d.SetId(c.Server)
+	if c.Name != "" {
+		d.SetId(fmt.Sprintf("%s/%s", c.Server, c.Name))
+	} else {
+		d.SetId(c.Server)
+	}
 	return resourceArgoCDClusterRead(d, meta)
 }
 
 func resourceArgoCDClusterRead(d *schema.ResourceData, meta interface{}) error {
 	server := meta.(ServerInterface)
 	client := *server.ClusterClient
-	clusterQuery := &clusterClient.ClusterQuery{Server: d.Id()}
-	c, err := client.Get(context.Background(), clusterQuery)
+	c, err := client.Get(context.Background(), getClusterQueryFromID(d))
 	if err != nil {
 		return fmt.Errorf("could not get cluster information: %s", err)
 	}
@@ -66,10 +70,22 @@ func resourceArgoCDClusterUpdate(d *schema.ResourceData, meta interface{}) error
 func resourceArgoCDClusterDelete(d *schema.ResourceData, meta interface{}) error {
 	server := meta.(ServerInterface)
 	client := *server.ClusterClient
-	_, err := client.Delete(context.Background(), &clusterClient.ClusterQuery{Server: d.Id()})
+	_, err := client.Delete(context.Background(), getClusterQueryFromID(d))
 	if err != nil {
 		return fmt.Errorf("something went wrong during cluster deletion: %s", err)
 	}
 	d.SetId("")
 	return nil
+}
+
+func getClusterQueryFromID(d *schema.ResourceData) *clusterClient.ClusterQuery {
+	id := strings.Split(strings.TrimPrefix(d.Id(), "https://"), "/")
+	cq := &clusterClient.ClusterQuery{}
+	if len(id) > 1 {
+		cq.Name = id[len(id)-1]
+		cq.Server = fmt.Sprintf("https://%s", strings.Join(id[:len(id)-1], "/"))
+	} else {
+		cq.Server = d.Id()
+	}
+	return cq
 }
