f30d56276816abb96778f3f412c9a429a5f96d5a
Alexander Backlund <alex@alxb.se>
2022-02-24 20:53:45 +0100

    tests: add precheck function for feature support
    Useful for acceptance tests that should only be run when specific
    features are supported by ArgoCD version.
    
    If a feature is not supported the test is skipped, not failed.

--- argocd/provider_test.go
+++ argocd/provider_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/Masterminds/semver"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -39,5 +40,23 @@ func testAccPreCheck(t *testing.T) {
 	}
 	if v := os.Getenv("ARGOCD_INSECURE"); v == "" {
 		t.Fatal("ARGOCD_INSECURE should be set for acceptance tests")
+	}
+}
+
+func testAccPreCheckFeatureSupported(t *testing.T, feature int) {
+	v := os.Getenv("ARGOCD_VERSION")
+	if v == "" {
+		t.Skip("ARGOCD_VERSION must be set set for feature supported acceptance tests")
+	}
+	serverVersion, err := semver.NewVersion(v)
+	if err != nil {
+		t.Fatalf("could not parse ARGOCD_VERSION as semantic version: %s", v)
+	}
+	versionConstraint, ok := featureVersionConstraintsMap[feature]
+	if !ok {
+		t.Fatal("feature constraint is not handled by the provider")
+	}
+	if i := versionConstraint.Compare(serverVersion); i == 1 {
+		t.Skipf("version %s does not support feature", v)
 	}
 }
