d582f9bf0ae43b478e23d83f2f9b5dff66dd6fef
Karl Schriek <karl.schriek@bma.network>
2022-10-27 15:30:59 +0200

    remove source_namespaces from schemas V0 and V1

--- argocd/schema_project.go
+++ argocd/schema_project.go
@@ -145,11 +145,6 @@ func projectSpecSchemaV0() *schema.Schema {
 					Required: true,
 					Elem:     &schema.Schema{Type: schema.TypeString},
 				},
-				"source_namespaces": {
-					Type:     schema.TypeList,
-					Required: true,
-					Elem:     &schema.Schema{Type: schema.TypeString},
-				},
 				"sync_window": {
 					Type:     schema.TypeList,
 					Optional: true,
@@ -359,11 +354,6 @@ func projectSpecSchemaV1() *schema.Schema {
 					},
 				},
 				"source_repos": {
-					Type:     schema.TypeList,
-					Required: true,
-					Elem:     &schema.Schema{Type: schema.TypeString},
-				},
-				"source_namespaces": {
 					Type:     schema.TypeList,
 					Required: true,
 					Elem:     &schema.Schema{Type: schema.TypeString},
