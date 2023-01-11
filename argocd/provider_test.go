d9989c59f2f8d2a2214dc57cbff00705112ef234
OneMatchFox <878612+onematchfox@users.noreply.github.com>
2023-01-11 13:35:10 +0100

    tests: remove duplicate `v` from log message

--- argocd/provider_test.go
+++ argocd/provider_test.go
@@ -77,6 +77,6 @@ func testAccPreCheckFeatureNotSupported(t *testing.T, feature int) {
 		t.Fatal("feature constraint is not handled by the provider")
 	}
 	if i := versionConstraint.Compare(serverVersion); i != 1 {
-		t.Skipf("not running test if feature is already supported (v%s)", v)
+		t.Skipf("not running test if feature is already supported (%s)", v)
 	}
 }
