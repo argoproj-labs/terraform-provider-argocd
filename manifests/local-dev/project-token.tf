8f3f8ba3bac88764f519341c08d1268f4376cf8f
OneMatchFox <878612+onematchfox@users.noreply.github.com>
2023-03-29 13:20:04 +0200

    chore: add short lived token to `local-dev` setup

--- manifests/local-dev/project-token.tf
+++ manifests/local-dev/project-token.tf
@@ -3,3 +3,11 @@ resource "argocd_project_token" "long" {
   role        = "foo"
   description = "long lived token"
 }
+
+resource "argocd_project_token" "renew_before" {
+  project      = argocd_project.foo.metadata.0.name
+  role         = "foo"
+  description  = "auto-renewing short lived token"
+  expires_in   = "24h"
+  renew_before = "10h"
+}
