274d72afe6a57da0ffc7682e54f412ca3ba29fb5
Andrew Hubler <ahubler@rapidsos.com>
2023-09-14 09:45:34 -0400

    Add ApplicationSetApplicationsSyncPolicy feature

--- internal/features/features.go
+++ internal/features/features.go
@@ -13,6 +13,7 @@ const (
 	ApplicationSet
 	ApplicationSetProgressiveSync
 	ManagedNamespaceMetadata
+	ApplicationSetApplicationsSyncPolicy
 )
 
 type FeatureConstraint struct {
@@ -21,10 +22,11 @@ type FeatureConstraint struct {
 }
 
 var ConstraintsMap = map[Feature]FeatureConstraint{
-	ExecLogsPolicy:                {"exec/logs RBAC policy", semver.MustParse("2.4.4")},
-	ProjectSourceNamespaces:       {"project source namespaces", semver.MustParse("2.5.0")},
-	MultipleApplicationSources:    {"multiple application sources", semver.MustParse("2.6.3")}, // Whilst the feature was introduced in 2.6.0 there was a bug that affects refresh of applications (and hence `wait` within this provider) that was only fixed in https://github.com/argoproj/argo-cd/pull/12576
-	ApplicationSet:                {"application sets", semver.MustParse("2.5.0")},
-	ApplicationSetProgressiveSync: {"progressive sync (`strategy`)", semver.MustParse("2.6.0")},
-	ManagedNamespaceMetadata:      {"managed namespace metadsata", semver.MustParse("2.6.0")},
+	ExecLogsPolicy:                       {"exec/logs RBAC policy", semver.MustParse("2.4.4")},
+	ProjectSourceNamespaces:              {"project source namespaces", semver.MustParse("2.5.0")},
+	MultipleApplicationSources:           {"multiple application sources", semver.MustParse("2.6.3")}, // Whilst the feature was introduced in 2.6.0 there was a bug that affects refresh of applications (and hence `wait` within this provider) that was only fixed in https://github.com/argoproj/argo-cd/pull/12576
+	ApplicationSet:                       {"application sets", semver.MustParse("2.5.0")},
+	ApplicationSetProgressiveSync:        {"progressive sync (`strategy`)", semver.MustParse("2.6.0")},
+	ManagedNamespaceMetadata:             {"managed namespace metadsata", semver.MustParse("2.6.0")},
+	ApplicationSetApplicationsSyncPolicy: {"application set level application sync policy", semver.MustParse("2.8.0")},
 }
