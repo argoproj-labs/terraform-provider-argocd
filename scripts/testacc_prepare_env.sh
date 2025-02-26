85d1b81c8da5c6070cd2528f8c504c2bd582e7c2
Nathanael Liechti <technat@technat.ch>
2025-02-26 14:44:40 +0100

    feat: streamline testing configuration & let workflows use makefile
    Signed-off-by: Nathanael Liechti <technat@technat.ch>

--- scripts/testacc_prepare_env.sh
+++ scripts/testacc_prepare_env.sh
@@ -4,9 +4,6 @@ set -e
 
 export PATH=$PATH:.
 
-argocd_version=${ARGOCD_VERSION:-v2.12.8}
-k8s_version=${ARGOCD_KUBERNETES_VERSION:-v1.27.11}
-
 echo "\n--- Clearing current kube context\n"
 kubectl config unset current-context
 
@@ -14,7 +11,7 @@ echo "\n--- Kustomize sanity checks\n"
 kustomize version || exit 1
 
 echo "\n--- Create Kind cluster\n"
-kind create cluster --name argocd --config scripts/kind-config.yml --image kindest/node:$k8s_version
+kind create cluster --config scripts/kind-config.yml 
 
 echo "\n--- Kind sanity checks\n"
 kubectl get nodes -o wide
@@ -22,23 +19,9 @@ kubectl get pods --all-namespaces -o wide
 kubectl get services --all-namespaces -o wide
 
 echo "\n--- Fetch ArgoCD installation manifests\n"
-curl https://raw.githubusercontent.com/argoproj/argo-cd/$argocd_version/manifests/install.yaml > manifests/install/argocd.yml
+curl https://raw.githubusercontent.com/argoproj/argo-cd/${ARGOCD_VERSION}/manifests/install.yaml > manifests/install/argocd.yml
 
-if [ -z "${ARGOCD_CI}" ]; then
-  echo "\n--- Load local container images from into Kind (local development only)\n"
-  docker pull quay.io/argoproj/argocd:$argocd_version
-  kind load docker-image quay.io/argoproj/argocd:$argocd_version --name argocd
-  
-  dex_version=$(cat manifests/install/argocd.yml| grep "image: ghcr.io/dexidp/dex" | cut -d":" -f3)
-  docker pull ghcr.io/dexidp/dex:$dex_version
-  kind load docker-image ghcr.io/dexidp/dex:$dex_version --name argocd
-  
-  redis_version=$(cat manifests/install/argocd.yml| grep "image: redis" | cut -d":" -f3)
-  docker pull redis:$redis_version
-  kind load docker-image redis:$redis_version --name argocd
-fi
-
-echo "\n--- Install ArgoCD $argocd_version\n"
+echo "\n--- Install ArgoCD ${ARGOCD_VERSION}\n"
 kustomize build manifests/install | kubectl apply -f - && \
   kubectl apply -f manifests/testdata/
 
