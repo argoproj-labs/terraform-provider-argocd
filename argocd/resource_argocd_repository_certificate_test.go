f74c9128a6e65a8e86ebf3021afdefca60066efe
Blake Pettersson <blake.pettersson@gmail.com>
2025-07-16 10:24:16 +0200

    chore: migrate to terraform-plugin-testing
    It gets pretty messy trying to mix `terraform-plugin-testing` with the
    older `terraform-plugin-sdk` testing framework, so let's just use
    `terraform-plugin-testing` for both the old resources as well as the new ones.
    
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>

--- argocd/resource_argocd_repository_certificate_test.go
+++ argocd/resource_argocd_repository_certificate_test.go
@@ -10,8 +10,8 @@ import (
 	"testing"
 
 	"github.com/argoproj-labs/terraform-provider-argocd/internal/testhelpers"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/stretchr/testify/assert"
 )
 
