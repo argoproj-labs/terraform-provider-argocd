afb8a07f4b73fbeb339d17aedf0f62abc87dc142
Blake Pettersson <blake.pettersson@gmail.com>
2025-08-14 02:45:04 -1000

    chore: set defaults in testmain (#706)
    * chore: set defaults in testmain
    
    This keeps the default configuration in one place, and at the same time
    allows for acceptance tests to not to have to use the make targets to
    get the default values set (useful in e.g the IDE).
    
    This also flips `USE_TESTCONTAINERS` to have a default of `true` (for now
    we don't do that in CI). This is something which can be discussed to see
    if that is desired by others.
    
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>
    
    * refactor: some minor tweaks
    
    * add new line when printing
    * inline `runTestSuite` logic
    
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>
    
    ---------
    
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>

--- internal/testhelpers/suite.go
+++ internal/testhelpers/suite.go
@@ -2,6 +2,7 @@ package testhelpers
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"sync"
 	"testing"
@@ -15,10 +16,25 @@ var (
 
 // TestMain is a helper function to be used in test files' TestMain functions
 func TestMain(m *testing.M) {
+	envDefaultValue("ARGOCD_AUTH_USERNAME", "admin")
+	envDefaultValue("ARGOCD_AUTH_PASSWORD", "acceptancetesting")
+	envDefaultValue("ARGOCD_SERVER", "127.0.0.1:8080")
+	envDefaultValue("ARGOCD_INSECURE", "true")
+	envDefaultValue("USE_TESTCONTAINERS", "true")
+	envDefaultValue("K3S_VERSION", "v1.31.6-k3s1")
+	envDefaultValue("ARGOCD_VERSION", "v3.0.0")
+
 	if os.Getenv("USE_TESTCONTAINERS") == "true" {
-		SetupTestSuite(m)
+		os.Exit(runTestSuite(m))
 	} else {
 		os.Exit(m.Run())
+	}
+}
+
+func envDefaultValue(envvar, defaultValue string) {
+	if v := os.Getenv(envvar); v == "" {
+		fmt.Printf("environment variable %s not set; using %s as default value\n", envvar, defaultValue)
+		_ = os.Setenv(envvar, defaultValue)
 	}
 }
 
@@ -26,12 +42,6 @@ const (
 	// DefaultTestTimeout is the default timeout for test setup
 	DefaultTestTimeout = 15 * time.Minute
 )
-
-// SetupTestSuite sets up a shared test environment for all acceptance tests
-func SetupTestSuite(m *testing.M) {
-	code := runTestSuite(m)
-	os.Exit(code)
-}
 
 func runTestSuite(m *testing.M) int {
 	ctx, cancel := context.WithTimeout(context.Background(), DefaultTestTimeout)
